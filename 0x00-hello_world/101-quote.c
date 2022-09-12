89e40303823e288033e2776a711c9daca585463b
Oshiorai <Jennifer Oshiobugie>
2022-09-12 06:17:08 -0700

    Tasks 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,13 @@
-#include <unistd.h>
+#include <stdio.h>
+
 /**
-
  *main-program entry point.
-
  *
-
- *Return: 1 on success.
-
+ *Return:1 on success.
  */
-
 int main(void)
 {
-    write(2,"and that piece of art is useful\"-Dora Korpar,2015-10-19\n",59);
-    return (1)
+printf(2, "and that piece of art is useful\"-Dora Korpar,2015-10-19\n", 59);
+return (1);
 
 }
